ef0ff08a386cb1a7c73339831a131db2460a2c39
Jack Coventry <jackcoventry@gmail.com>
2026-02-16 14:18:54 +0000

    Added local fonts from google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,21 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Special+Gothic+Condensed+One&family=Special+Gothic+Expanded+One&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>
         <main>{children}</main>
         <Footer />
